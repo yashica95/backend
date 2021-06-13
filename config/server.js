69cfa820efa53bb3298722ff7fe966d229d5179d
Yashica <yashica.iitd@gmail.com>
2021-06-14 00:46:45 +0530

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3de21845e62bf6ab6b0a4470cd846794'),
+    },
+  },
+});
